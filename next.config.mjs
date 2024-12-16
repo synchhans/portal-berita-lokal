69fbce72b45807334a63c370d308a10feee224b5
Mrglxor <muhamadfarhan.inc@gmail.com>
2024-12-16 12:39:32 +0700

    fix config nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    outputFileTracingExcludes: {
-      "/api/**/*": ["**/*.js", "**/*.ts"],
-    },
-  },
 };
 
 export default nextConfig;
