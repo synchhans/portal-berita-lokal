d544d6c2f120ce8bc20fc728ba3436e84fe55086
Mrglxor <muhamadfarhan.inc@gmail.com>
2024-12-17 13:51:59 +0700

    fix sitemap dinamis

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,61 +5,6 @@ import NewsModel from "../../utils/model/News";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://kabarlokal.vercel.app";
 
-  const staticRoutes = [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/peristiwa-lokal`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/ekonomi`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/bisnis`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/politik`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/kesehatan`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/pendidikan`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/budaya`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/pariwisata`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/teknologi`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/komunitas`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/sosial`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/properti`,
-      lastModified: new Date(),
-    },
-  ];
-
   await connectToDB();
   const news = await NewsModel.find({ status: "approved" })
     .sort({ createdAt: -1 })
@@ -70,5 +15,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     url: `${baseUrl}/${article.category.toLowerCase()}/${article.title_seo}`,
   }));
 
-  return [...staticRoutes, ...dynamicRoutes];
+  return [...dynamicRoutes];
 }
